58d41b573c6ef0ad803069b7b7ea37431648b03d
Muhammadwahyu1212 <muhammad123wahyu123@gmail.com>
2026-05-21 21:51:51 +0700

    refactor: move menu data into separate module

--- weekly1.js
+++ weekly1.js
@@ -1,45 +1,12 @@
 const readline = require("node:readline");
 const calculateResult = require("./utils/calculate");
+const menu = require("./data/menu");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 console.clear();
-
-const menu = [{
-    name: "Esteh Melati",
-    price: 15500
-}, {
-    name: "Thai Tea",
-    price: 22000
-}, {
-    name: "Matcha Bunga Melati",
-    price: 29500
-}, {
-    name: "Cincau Esteh Susu",
-    price: 21000
-}, {
-    name: "Esteh Matcha Latte",
-    price: 29500
-}, {
-    name: "Esteh Lychee",
-    price: 20500
-}, {
-    name: "Esteh Susu Nusantara",
-    price: 20500
-}, {
-    name: "Matcha Sakura",
-    price: 26000
-}, {
-    name: "Chizu Matcha",
-    price: 29500
-}, {
-    name: "Chizu Taro",
-    price: 29500
-}];
-
-
 
 function mainMenu() {
     console.log(`
