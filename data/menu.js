0a68beedef29a63461b5655498d85658f45e549c
Muhammadwahyu1212 <muhammad123wahyu123@gmail.com>
2026-05-21 21:48:03 +0700

    refactor :move menu data into separate module

---
+++ data/menu.js
@@ -0,0 +1,33 @@
+const menu = [{
+    name: "Esteh Melati",
+    price: 15500
+}, {
+    name: "Thai Tea",
+    price: 22000
+}, {
+    name: "Matcha Bunga Melati",
+    price: 29500
+}, {
+    name: "Cincau Esteh Susu",
+    price: 21000
+}, {
+    name: "Esteh Matcha Latte",
+    price: 29500
+}, {
+    name: "Esteh Lychee",
+    price: 20500
+}, {
+    name: "Esteh Susu Nusantara",
+    price: 20500
+}, {
+    name: "Matcha Sakura",
+    price: 26000
+}, {
+    name: "Chizu Matcha",
+    price: 29500
+}, {
+    name: "Chizu Taro",
+    price: 29500
+}];
+
+module.exports = menu;
