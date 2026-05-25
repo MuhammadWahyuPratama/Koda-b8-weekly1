62d25014e2a99218677615e38f20888a1613b544
Muhammadwahyu1212 <muhammad123wahyu123@gmail.com>
2026-05-25 22:14:57 +0700

    configure eslint and node test setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node },
+    files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: {globals: { ...globals.node,...globals.jest}},
     rules: {
       semi: ["error", "always"],
       indent: ["error", 2]
