f3ae71bcb9b0207acadd87101b5b54b744549156
Muhammadwahyu1212 <muhammad123wahyu123@gmail.com>
2026-05-26 08:53:36 +0700

    dosc: add jsdocs in calculate.js

--- utils/calculate.js
+++ utils/calculate.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * @param {object[]} cart list of cart items
+ * @param {number} cart[].subtotal price for each item
+ * @returns {number} total amount of all cart items
+ */
+
 function calculateResult(cart) {
   return cart.reduce((result, item) => {
     return result + item.subtotal;
